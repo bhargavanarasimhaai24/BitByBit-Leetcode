76963c463f45469f007c0e2e79ce959cec1092ad
Kopparapu Bhargava Narasimha <bhargavanarasimha.ai24@bmsce.ac.in>
2025-11-22 11:29:17 +0530

    Duplicates

---
+++ 287.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        slow = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
