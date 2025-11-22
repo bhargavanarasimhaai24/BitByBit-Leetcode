1618db6e9ba0ce49c67fead3196caf0a0bf8ac53
Kopparapu Bhargava Narasimha <bhargavanarasimha.ai24@bmsce.ac.in>
2025-11-22 11:19:45 +0530

    Add 2 Numbers

---
+++ 2.py
@@ -0,0 +1,23 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy=ListNode()
+        curr=dummy
+        carry=0
+        
+        while l1 or l2 or carry:
+            v1=l1.val if l1 else 0
+            v2=l2.val if l2 else 0  
+            s = v1+v2+carry
+            carry=s//10
+            curr.next=ListNode(s%10)
+            curr=curr.next
+            if l1:
+                l1=l1.next
+            if l2: 
+                l2=l2.next
+        
+        return dummy.next
